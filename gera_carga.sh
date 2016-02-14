b3f6358f9b85e7cc1e6fdabcfd7742902e9e6573
Bruno Xavier <bgxavier@gmail.com>
2016-02-13 23:26:47 -0200

    .

--- gera_carga.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {0..30}
-do
-	rally task start /opt/openstack_benchmarks/boot.json;
-	sleep 10;
-	ssh stack@10.32.45.217 'rm -f /opt/stack/data/nova/instances/_base/*';
-done
